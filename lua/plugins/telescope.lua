eff888198506d626504ad37098e9dc60d7686575
Robin <rtmbaertschi007@gmail.com>
2024-11-10 16:17:32 +0100

    changes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,40 +1,40 @@
 return {
-	'nvim-telescope/telescope.nvim', branch = '0.1.x',
-	dependencies = { 'nvim-lua/plenary.nvim',
-        { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
-        { -- If encountering errors, see telescope-fzf-native README for installation instructions
-            'nvim-telescope/telescope-fzf-native.nvim',
+	"nvim-telescope/telescope.nvim",
+	branch = "0.1.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
+		{ -- If encountering errors, see telescope-fzf-native README for installation instructions
+			"nvim-telescope/telescope-fzf-native.nvim",
 
-            -- `build` is used to run some command when the plugin is installed/updated.
-            -- This is only run then, not every time Neovim starts up.
-            build = 'make',
+			-- `build` is used to run some command when the plugin is installed/updated.
+			-- This is only run then, not every time Neovim starts up.
+			build = "make",
 
-            -- `cond` is a condition used to determine whether this plugin should be
-            -- installed and loaded.
-            cond = function()
-              return vim.fn.executable 'make' == 1
-            end,
-        },
-        { 'nvim-telescope/telescope-ui-select.nvim' },
-    },
-    config = function()
-        require('telescope').setup {
-            extensions = {
-                ['ui-select'] = {
-                    require('telescope.themes').get_dropdown(),
-                },
-            },
-            defaults = {
-                file_ignore_patterns = {
-                    'target', -- Rust
-                    'build', -- Most build systems
-                    'builddir', -- Meson
-                },
-            }
-        }
+			-- `cond` is a condition used to determine whether this plugin should be
+			-- installed and loaded.
+			cond = function()
+				return vim.fn.executable("make") == 1
+			end,
+		},
+		{ "nvim-telescope/telescope-ui-select.nvim" },
+	},
+	config = function()
+		require("telescope").setup({
+			extensions = {
+				["ui-select"] = {
+					require("telescope.themes").get_dropdown(),
+				},
+			},
+			defaults = {
+				file_ignore_patterns = {
+					"target", -- Rust
+				},
+			},
+		})
 
-        local ts = require('telescope')
-        ts.load_extension('fzf')
-        ts.load_extension('ui-select')
-    end
+		local ts = require("telescope")
+		ts.load_extension("fzf")
+		ts.load_extension("ui-select")
+	end,
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -22,9 +22,9 @@ return { -- Highlight, edit, and navigate code
 			-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
 			--  If you are experiencing weird indenting issues, add the language to
 			--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-			additional_vim_regex_highlighting = { "ruby" },
+			additional_vim_regex_highlighting = { "ruby", "zig" },
 		},
-		indent = { enable = true, disable = { "ruby" } },
+		indent = { enable = true, disable = { "ruby", "zig" } },
 	},
 	config = function(_, opts)
 		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
