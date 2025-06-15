b6b70d15b05da965aed443d3b25e00b38478ea40
RoBaertschi <rtmbaertschi007@gmail.com>
2025-06-15 12:19:44 +0200

    nvim-cmp: add buffer source

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -11,6 +11,7 @@ return { -- Autocompletion
 		--  into multiple repos for maintenance purposes.
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-buffer",
 	},
 	config = function()
 		-- See `:help cmp`
@@ -86,6 +87,7 @@ return { -- Autocompletion
 				{ name = "nvim_lsp" },
 				{ name = "luasnip" },
 				{ name = "path" },
+				{ name = "buffer" },
 			},
 		})
 	end,
