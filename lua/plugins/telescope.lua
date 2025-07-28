df282645aba4d6e7a536ea75783237a47081f356
RoBaertschi <rtmbaertschi007@gmail.com>
2025-07-28 10:40:49 +0200

    use my own telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,17 +5,19 @@ return {
 		"nvim-lua/plenary.nvim",
 		{ "nvim-tree/nvim-web-devicons", enabled = vim.g.have_nerd_font },
 		{ -- If encountering errors, see telescope-fzf-native README for installation instructions
-			"nvim-telescope/telescope-fzf-native.nvim",
+			"RoBaertschi/telescope-fzf-native.nvim",
 
 			-- `build` is used to run some command when the plugin is installed/updated.
 			-- This is only run then, not every time Neovim starts up.
-			build = "make",
-
-			-- `cond` is a condition used to determine whether this plugin should be
-			-- installed and loaded.
-			cond = function()
-				return vim.fn.executable("make") == 1
-			end,
+			-- build = function()
+			-- 	if vim.fn.executable("make") == 1 then
+			-- 		vim.system({ "make" })
+			-- 		return
+			-- 	end
+			-- 	vim.system({ "cmake", "-S.", "-Bbuild", "-DCMAKE_BUILD_TYPE=Release" })
+			-- 	vim.system({ "cmake", "--build", "build", "--config", "Release" })
+			-- end,
+			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
 		},
 		{ "nvim-telescope/telescope-ui-select.nvim" },
 	},
